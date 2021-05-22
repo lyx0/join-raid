f1c38585c277e2a18789b6b13490bb99adb85330
lyx0 <stefan.parfuss@protonmail.ch>
2021-05-22 20:31:50 +0200

    add teischente, add backups

--- bot.js
+++ bot.js
@@ -22,9 +22,10 @@ client.use(new Twitch.SlowModeRateLimiter(client, 2));
 // Initialize Client
 client.initialize = async () => {
     await client.join('uudelleenkytkeytynyt')
-//    await client.join('teischente')
+    await client.join('teischente')
     await client.join('smaczny')
     await client.join('tuimeep')
+    await client.join('nouryqt')
 
     await client.connect();
 };
@@ -39,35 +40,36 @@ client.on('close', (error) => {
 // Connection was successful
 client.on('ready', () => {
     console.log('Successfully connected to chat');
+    client.say('nouryqt', 'join-raid running dankCircle');
     client.say('uudelleenkytkeytynyt', 'Running');
 });
 
 
 // #teischente
-//client.on('PRIVMSG', (msg) => {
-//    if (msg.displayName === 'HuwoBot' &&
-//        msg.messageText.startsWith('A Raid Event at Level') && 
-//        msg.channelName.toLowerCase() === 'teischente') {
-//        let users = fs.readFileSync('teischente.txt').toString().split('\n').filter(u => u).map(u => ' @' + u);
-//        // console.log(users)
-//        client.say(msg.channelName, `RAID DETECTED pajaGIGA 🚨 ${users}`)
-//    }
-//})
-//
-//client.on('PRIVMSG', (msg) => {
-//    if ((msg.messageText.startsWith('()remindme') || 
-//        msg.messageText.startsWith('()join')) &&
-//        msg.channelName.toLowerCase() === 'teischente') {
-//        let nameToAdd = msg.displayName.toString()
-//        fs.appendFile('teischente.txt', `\n${nameToAdd}`, function (err) {
-//            if (err) {
-//                client.say(msg.channelName, 'Something went wrong FeelsBadMan')
-//            } else {
-//                client.say(msg.channelName, `@${nameToAdd}, I will now ping you when a raid happens :)`)
-//            }
-//        })
-//    }
-//})
+client.on('PRIVMSG', (msg) => {
+    if (msg.displayName === 'HuwoBot' &&
+        msg.messageText.startsWith('A Raid Event at Level') && 
+        msg.channelName.toLowerCase() === 'teischente') {
+        let users = fs.readFileSync('teischente.txt').toString().split('\n').filter(u => u).map(u => ' @' + u);
+        // console.log(users)
+        client.say(msg.channelName, `RAID DETECTED pajaGIGA 🚨 ${users}`)
+    }
+})
+
+client.on('PRIVMSG', (msg) => {
+    if ((msg.messageText.startsWith('()remindme') || 
+        msg.messageText.startsWith('()join')) &&
+        msg.channelName.toLowerCase() === 'teischente') {
+        let nameToAdd = msg.displayName.toString()
+        fs.appendFile('teischente.txt', `\n${nameToAdd}`, function (err) {
+            if (err) {
+                client.say(msg.channelName, 'Something went wrong FeelsBadMan')
+            } else {
+                client.say(msg.channelName, `@${nameToAdd}, I will now ping you when a raid happens :)`)
+            }
+        })
+    }
+})
 
 // #smaczny
 client.on('PRIVMSG', (msg) => {
